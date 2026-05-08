27937666a825798528a6d3a48324091565e789e0
bianca <ghostekkjk@gmail.com>
2026-05-08 14:13:11 -0300

    arruma questao model e controller para o novo banco

--- src/models/QuestaoModel.js
+++ src/models/QuestaoModel.js
@@ -1,21 +1,60 @@
 import prisma from '../lib/services/prismaClient.js';
 
 export default class QuestaoModel {
-    constructor({ id = null, enunciado, enunciado_en } = {}) {
+    constructor({
+        id = null,
+        pergunta,
+        pergunta_en,
+        opcaoA,
+        opcaoA_en,
+        opcaoB,
+        opcaoB_en,
+        opcaoC,
+        opcaoC_en,
+        opcaoD,
+        opcaoD_en,
+        opcaoE,
+        opcaoE_en,
+        respostaCorreta,
+        explicacao,
+        explicacao_en,
+    } = {}) {
         this.id = id;
-        this.enunciado = enunciado;
-        this.enunciado_en = enunciado_en;
+        this.pergunta = pergunta;
+        this.pergunta_en = pergunta_en;
+        this.opcaoA = opcaoA;
+        this.opcaoA_en = opcaoA_en;
+        this.opcaoB = opcaoB;
+        this.opcaoB_en = opcaoB_en;
+        this.opcaoC = opcaoC;
+        this.opcaoC_en = opcaoC_en;
+        this.opcaoD = opcaoD;
+        this.opcaoD_en = opcaoD_en;
+        this.opcaoE = opcaoE;
+        this.opcaoE_en = opcaoE_en;
+        this.respostaCorreta = respostaCorreta;
+        this.explicacao = explicacao;
+        this.explicacao_en = explicacao_en;
     }
 
     async criar() {
         return prisma.questao.create({
             data: {
-                enunciado: this.enunciado,
-                enunciado_en: this.enunciado_en,
-            },
-            include: {
-                alternativas: true,
-                respostas: true,
+                pergunta: this.pergunta,
+                pergunta_en: this.pergunta_en,
+                opcaoA: this.opcaoA,
+                opcaoA_en: this.opcaoA_en,
+                opcaoB: this.opcaoB,
+                opcaoB_en: this.opcaoB_en,
+                opcaoC: this.opcaoC,
+                opcaoC_en: this.opcaoC_en,
+                opcaoD: this.opcaoD,
+                opcaoD_en: this.opcaoD_en,
+                opcaoE: this.opcaoE,
+                opcaoE_en: this.opcaoE_en,
+                respostaCorreta: this.respostaCorreta,
+                explicacao: this.explicacao,
+                explicacao_en: this.explicacao_en,
             },
         });
     }
@@ -24,12 +63,21 @@ export default class QuestaoModel {
         return prisma.questao.update({
             where: { id: this.id },
             data: {
-                enunciado: this.enunciado,
-                enunciado_en: this.enunciado_en,
-            },
-            include: {
-                alternativas: true,
-                respostas: true,
+                pergunta: this.pergunta,
+                pergunta_en: this.pergunta_en,
+                opcaoA: this.opcaoA,
+                opcaoA_en: this.opcaoA_en,
+                opcaoB: this.opcaoB,
+                opcaoB_en: this.opcaoB_en,
+                opcaoC: this.opcaoC,
+                opcaoC_en: this.opcaoC_en,
+                opcaoD: this.opcaoD,
+                opcaoD_en: this.opcaoD_en,
+                opcaoE: this.opcaoE,
+                opcaoE_en: this.opcaoE_en,
+                respostaCorreta: this.respostaCorreta,
+                explicacao: this.explicacao,
+                explicacao_en: this.explicacao_en,
             },
         });
     }
@@ -41,26 +89,18 @@ export default class QuestaoModel {
     static async buscarTodos(filtros = {}) {
         const where = {};
 
-        if (filtros.enunciado) {
-            where.enunciado = { contains: filtros.enunciado, mode: 'insensitive' };
+        if (filtros.pergunta) {
+            where.pergunta = { contains: filtros.pergunta, mode: 'insensitive' };
         }
 
         return prisma.questao.findMany({
             where,
-            include: {
-                alternativas: true,
-                respostas: true,
-            },
         });
     }
 
     static async buscarPorId(id) {
         const data = await prisma.questao.findUnique({
             where: { id },
-            include: {
-                alternativas: true,
-                respostas: true,
-            },
         });
         if (!data) {
             return null;
