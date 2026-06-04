de18532b1fdcdd8bf2e6cac13d5fbcb60c55ae88
gabi emi <gabiemi2021@gmail.com>
2026-06-04 18:01:55 -0300

    arruma erro

--- src/routes/integracaoRoute.js
+++ src/routes/integracaoRoute.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import {
     buscarTodosIntegrados,
-    buscarLivroIntegradoPorId
+    buscarLivroPorId
 } from '../controllers/integracaoController.js';
 
 const router = express.Router();
